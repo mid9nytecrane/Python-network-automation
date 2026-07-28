2ba4fc7bca0b46e74ac5c2237c2b2f64fa6c16d9
mid9nytecrane <sheriffsakara41@gmail.com>
2026-07-28 22:02:26 +0000

    delete some files

--- test_script.py
+++
@@ -1,7 +0,0 @@
-import subprocess 
-
-def list_wifi_networks():
-    cmd = "netsh wlan show networks"
-    result = subprocess.check_output(cmd, shell=True, text=True)
-    print(result)
-list_wifi_networks
